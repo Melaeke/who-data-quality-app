db0f86f510e5d6ec67730dd556e934b5075b3ed1
Erik Arenhill <erik@dhis2.org>
2019-08-13 10:49:11 +0200

    chore: add cli-style default eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,5 @@
+const { config } = require('@dhis2/cli-style')
+
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-	},
-    "extends": "eslint:recommended",
-    "parserOptions": {
-		"ecmaVersion": 2017,
-		"sourceType": "module",
-		"ecmaFeatures": {
-			"experimentalObjectRestSpread": true
-		}
-    },
-    "rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
-		"no-console": "off",
-		"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-	},
-	"globals": {
-		"angular": true,
-		"require": true,
-		"d3": true
-	}
-};
+    extends: [config.eslint],
+}
