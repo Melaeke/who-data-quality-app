784f04b8e61b634198eb65f2a1f3a6ab64f6eed4
Erik Arenhill <erik@dhis2.org>
2019-08-14 10:23:02 +0200

    test: uncomment/enable smoke test of App rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 import { Provider } from 'react-redux'
 import { store } from './store'
 
-import App from './App';
+import App from './App'
 
 // Mock headerbar, as the current version requires REACT_APP_DHIS2_BASE_URL to be set
 //jest.mock('@dhis2/ui-widgets/HeaderBar', () => <div />)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <></>
-    /*<Provider store={store}>
-      <App url={''} appName="WHO Data Quality Tool" apiVersion={32} />
-    </Provider>*/
-  , div);
-
-  ReactDOM.unmountComponentAtNode(div);
-});
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+})
